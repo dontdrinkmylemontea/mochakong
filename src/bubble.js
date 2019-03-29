8c26e29e21aaddd151586474b8921135871f1972
lxy <492861128@qq.com>
2019-03-29 14:49:52 +0800

    add coverage test

--- src/bubble.js
+++ src/bubble.js
@@ -1,14 +1,12 @@
-module.export = {
-  bubble: function(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      for (let j = 0; j < i; j++) {
-        if (array[j] > array[j + 1]) {
-          var temp = array[j];
-          array[j] = array[j + 1];
-          array[j + 1] = temp;
-        }
+exports.bubble = function(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (array[j] > array[j + 1]) {
+        var temp = array[j];
+        array[j] = array[j + 1];
+        array[j + 1] = temp;
       }
     }
-    return array;
   }
+  return array;
 };
---
+++ test/coverage.test.js
@@ -0,0 +1,14 @@
+var assert = require("assert");
+var bubble = require("../src/bubble").bubble;
+
+describe.only("bubble", function() {
+  var arr = [10, 5, 3, 7, 4];
+
+  it("should return array", function() {
+    assert.equal(Array.isArray(bubble(arr)), true);
+  });
+
+  it("should be sorted", function() {
+    assert.deepEqual(bubble(arr), [3, 4, 5, 7, 10]);
+  });
+});
